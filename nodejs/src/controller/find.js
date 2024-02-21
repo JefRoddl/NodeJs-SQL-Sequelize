364d9b3fa13e3482728bc0619168e5419eb7a042
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-02-20 22:43:11 -0300

    Inserção de clientes

--- nodejs/src/controller/find.js
+++ nodejs/src/controller/find.js
@@ -2,12 +2,17 @@ let sequelize = require('./../model/index')
 let Cliente = sequelize.import('./../model/client')
 
 module.exports = (req,res) => {
+    let msg = 0
+    if(req.query.msg == 1) {
+        msg = 1;
+    }
     Cliente
         .findAll()
         .then((clients) => {
             return res.render('client_list', {
                 title: "Lista de Clientes",
-                clients: clients
+                clients: clients,
+                msg: msg
             })
         })
         .catch((err) => {
